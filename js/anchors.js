e6d6de2896b35ecc04dade86709896b5cac65743
Vincent Rubinetti <vince.rubinetti@gmail.com>
2020-11-17 17:42:40 -0500

    update

--- js/anchors.js
+++ js/anchors.js
@@ -13,13 +13,13 @@ const createAnchors = () => {
     heading.append(link);
 
     // if first heading in the section, move id from heading to parent section
-    // if (heading === heading.parent.firstElementChild) {
-    //   let section = heading.closest("section");
-    //   if (section) {
-    //     section.id = heading.id;
-    //     heading.removeAttribute("id");
-    //   }
-    // }
+    if (heading === heading.parent.firstElementChild) {
+      let section = heading.closest("section");
+      if (section) {
+        section.id = heading.id;
+        heading.removeAttribute("id");
+      }
+    }
   }
 };
 
