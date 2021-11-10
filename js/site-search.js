0d247ede4afc471f6fff348b9e19160b8f890e16
Vincent Rubinetti <vince.rubinetti@gmail.com>
2021-11-10 17:43:30 -0500

    href to origin

--- js/site-search.js
+++ js/site-search.js
@@ -5,7 +5,7 @@
 const onSiteSearchSubmit = (event) => {
   event.preventDefault();
   const google = "https://www.google.com/search?q=site:";
-  const site = window.location.href;
+  const site = window.location.origin;
   const query = event.target.elements.query.value;
   window.location = google + site + " " + query;
 };
