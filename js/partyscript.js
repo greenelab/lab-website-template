0002727eb5950389ec1ef41c79999e295698b6a3
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-02 16:41:28 +0100

    Update partyscript.js

--- js/partyscript.js
+++ js/partyscript.js
@@ -3,12 +3,12 @@ $(document).ready(
      console.log("Ready!");
      var day_night = new Date().getHours();
      console.log(day_night);
-     if (day_night > 12 && day_night <= 17) {
+     if (day_night > 12 && day_night <= 18) {
        /*$('.advance-link>img')*/
         
         $(".portrait").each(function(){
              console.log($(this).attr("href"));
-             $(this).children(".portrait_image>img").attr('src',"/images/party_dog.jpg");
+             $(this).find("img").attr('src',"/images/party_dog.jpg");
         });
           
      }else{
