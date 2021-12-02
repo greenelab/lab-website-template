9a0da6dd07d6b303c785b0f5cd9753e73b6b5c23
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-02 16:00:33 +0100

    Update partyscript.js

--- js/partyscript.js
+++ js/partyscript.js
@@ -4,15 +4,16 @@
      console.log("Ready!");
      function check_image() {
       var day_night = new Date().getHours();
+      console.log($day_night);
       if (day_night > 12 && day_night <= 17) {
         /*$('.advance-link>img')*/
-        $("#portrait_image>img").css('src',"images/party_dog.jpg");
+        $(".portrait_image>img").css('src',"images/party_dog.jpg");
       }
       /*else if (day_night > 12 && day_night < 21) {
              $("#gilidimage").css('background-image',"url('http://day-image-here.png')");
         }*/
        else {
-            $("#portrait_image>img").css('src',"images/member.jpg");
+            $(".portrait_image>img").css('src',"images/member.jpg");
        } 
    }
 });
