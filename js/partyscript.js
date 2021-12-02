67188bd01821ad3750840e788ee90061ab8b6af7
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-02 16:17:47 +0100

    Update partyscript.js

--- js/partyscript.js
+++ js/partyscript.js
@@ -1,7 +1,8 @@
-$(document).ready(function(){
+$(document).ready(
+     function(){
      console.log("Ready!");
      var day_night = new Date().getHours();
      console.log(day_night);
-   }
+    
       
 });
