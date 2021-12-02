70f0213ad826273c4dad2268907eba7eca9e0287
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-02 17:00:44 +0100

    Update partyscript.js

--- js/partyscript.js
+++ js/partyscript.js
@@ -9,8 +9,8 @@ $(document).ready(
         $(".portrait").each(function(){
              console.log($(this).attr("href"));
              var img = $(this).find("img").attr("src");
-             console.log(img.replace(".jpg","_party.jpg"));
-             $(this).find("img").attr('src',"/images/party_dog.jpg");
+             var partyImage = img.replace(".jpg","_party.jpg");
+             $(this).find("img").attr('src',partyImage);
         });
           
      }else{
