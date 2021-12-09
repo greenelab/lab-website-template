3675109cf1eec248977946cc3f38cefabdb3e37a
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-09 14:05:58 +0100

    Update partyscript.js

--- custom_js/partyscript.js
+++ custom_js/partyscript.js
@@ -3,13 +3,10 @@ $(document).ready(
      function(){
           console.log("Ready!",partydates);
           var today = new Date();
-          var month = today.getMonth()
-          var day = today.getDay()
-          var partydates = JSON.parse(partydates);
-          for(var i = 0; i < partydates.length; i++) {
-               var obj = partydates[i];
-               console.log(obj.month, obj.day);
-          }
+          var month = today.getMonth()+1;
+          var day = today.getDay();
+          var md = day+"-"+month;
+          console.log(md);
           var day_night = 13;
           if (day_night > 12 && day_night <= 23) {
 
