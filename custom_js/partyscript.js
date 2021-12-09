18c5a51647218479daa04b507b7fce0da8da090b
JesseKerkvliet <9197634+JesseKerkvliet@users.noreply.github.com>
2021-12-09 14:52:07 +0100

    Update partyscript.js

--- custom_js/partyscript.js
+++ custom_js/partyscript.js
@@ -1,23 +1,20 @@
-var partydates = '["1-12","18-10","9-12"]';
+var partydates = '["1-12","18-10","14-4","18-1","27-12","8-11","9-12"]';
 $(document).ready(
      function(){
-          console.log("Ready!",partydates);
           var today = new Date();
           var month = today.getMonth()+1;
           var day = today.getDate();
           var md = day+"-"+month;
-          console.log(md);
-          var day_night = 13;
+          
           if (partydates.includes(md)){
-
                $(".portrait").each(function(){
                     var img =$(this).find("img").attr("src");
                     var partyimg = img.replace(".jpg","_party.jpg");
+                    
                     if (checkFileExist(partyimg)){
                          $(this).find("img").attr('src',partyimg);
                     }
-
-             
+                    
                });
           
           }
