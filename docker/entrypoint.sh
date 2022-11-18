ac5dc6d00cf0706716a59f84dd10e7fa557e9e0a
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-11-18 08:50:49 -0700

    Added watchdog, using it to run auto-cite whenever _data/sources.yaml changes

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # run citation generator
-python3 /usr/src/app/auto-cite/auto-cite.py
+watchmedo shell-command \
+    --command='python3 /usr/src/app/auto-cite/auto-cite.py' \
+    ./_data/sources.yaml &
 
 # serve the site in hot-reload mode forever
 bundle exec jekyll serve --host=0.0.0.0 --livereload --trace
