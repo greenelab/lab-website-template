d90f5364dab672779483d59117ff6d6ac76597eb
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2022-11-18 09:02:25 -0700

    Reboots Jekyll whenever _config.yaml changes. FIXME: doesn't hot-reload after reboot.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,5 +5,8 @@ watchmedo shell-command \
     --command='python3 /usr/src/app/auto-cite/auto-cite.py' \
     ./_data/sources.yaml &
 
-# serve the site in hot-reload mode forever
-bundle exec jekyll serve --host=0.0.0.0 --livereload --trace
+# serve the site in hot-reload mode forever.
+# reboot server whenever _config.yaml changes, as that's the only
+# thing jekyll can't hot-reload when it's modified
+watchmedo auto-restart --pattern='./_config.yaml' --signal SIGTERM \
+    -- bundle exec jekyll serve --host=0.0.0.0 --livereload --trace
