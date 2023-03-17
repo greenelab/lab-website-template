0825df2dc091e695d625ebfcf88b90fdaf3603b1
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-03-17 11:14:36 -0600

    Add Dockerization for locally building site and citations (#162)

---
+++ .docker/entrypoint.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# run jekyll serve in hot-reload mode.
+# rerun whenever _config.yaml changes (jekyll hot-reload doesn't work with this file).
+watchmedo auto-restart \
+    --debug-force-polling \
+    --patterns="_config.yaml" \
+    --signal SIGTERM \
+    -- bundle exec jekyll serve --open-url --force_polling --livereload --trace --host=0.0.0.0 &
+
+# run cite process.
+# rerun whenever _data files change.
+watchmedo shell-command \
+    --debug-force-polling \
+    --recursive \
+    --command="python3 _cite/cite.py" \
+    --patterns="_data/sources*;_data/orcid*;_data/pubmed*;_data/google-scholar*" \
