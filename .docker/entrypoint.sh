6e104632487b8c2960a658fa096163266cd8123e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2023-03-17 15:48:27 -0400

    restore faisal review comments from pr 162

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 
+# print folder contents for debugging
+echo "Contents:"
+echo ""
+ls
+echo ""
+
 # run jekyll serve in hot-reload mode.
 # rerun whenever _config.yaml changes (jekyll hot-reload doesn't work with this file).
 watchmedo auto-restart \
     --debug-force-polling \
     --patterns="_config.yaml" \
     --signal SIGTERM \
-    -- bundle exec jekyll serve --open-url --force_polling --livereload --trace --host=0.0.0.0 &
+    -- bundle exec jekyll serve --open-url --force_polling --livereload --trace --host=0.0.0.0 \
+    | sed 's/0.0.0.0/localhost/g' &
 
 # run cite process.
 # rerun whenever _data files change.
--- .docker/run.sh
+++ .docker/run.sh
@@ -6,21 +6,23 @@ IMAGE=lab-website-renderer:latest
 # name of running container
 CONTAINER=lab-website-renderer
 
-# choose platform
+# choose platform flag
 PLATFORM=""
 
-# default docker run command
+# default vars
 DOCKER_RUN="docker run"
+WORKING_DIR=$(pwd)
 
-# fix interactive on windows
+# windows-specific vars
 if [[ $OSTYPE == msys* ]]; then
     DOCKER_RUN="winpty docker run"
+    WORKING_DIR=$(cmd //c cd)
 fi
 
 # build docker image
 docker build ${PLATFORM} \
     --tag ${IMAGE} \
-    --file ./.docker/Dockerfile .
+    --file ./.docker/Dockerfile . && \
 
 # run built docker image
 ${DOCKER_RUN} ${PLATFORM} \
@@ -31,5 +33,5 @@ ${DOCKER_RUN} ${PLATFORM} \
     --tty \
     --publish 4000:4000 \
     --publish 35729:35729 \
-    --volume $PWD:/usr/src/app \
+    --volume "${WORKING_DIR}:/usr/src/app" \
     ${IMAGE} "$@"
