48521f8079e3e9588d9e4c8d54449d090fb3019e
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-07-28 16:53:13 -0400

    update orcid id choice behavior

--- _cite/plugins/orcid.py
+++ _cite/plugins/orcid.py
@@ -1,7 +1,7 @@
 import json
 from urllib.request import Request, urlopen
 from util import *
-from manubot.cite.handlers import prefix_to_handler as manubot_prefixes
+from manubot.cite.handlers import prefix_to_handler as manubot_citable
 
 
 def main(entry):
@@ -33,36 +33,43 @@ def main(entry):
     # list of sources to return
     sources = []
 
-    # go through response structure and pull out ids e.g. doi:1234/56789
-    for work in response:
-        # get list of ids
-        ids = []
-        for summary in get_safe(work, "work-summary", []):
-            ids = ids + get_safe(summary, "external-ids.external-id", [])
-
-        # find first id of particular "relationship" type
-        _id = next(
-            (
-                id
-                for id in ids
-                if get_safe(id, "external-id-relationship", "")
-                in ["self", "version-of", "part-of"]
-            ),
-            ids[0] if len(ids) > 0 else None,
-        )
-
-        if _id == None:
-            continue
-
-        # get id and id-type from response
+    # pick id to cite for source, return blank if does not meet certain criteria
+    def get_id(_id):
+        # split id into parts
         id_type = get_safe(_id, "external-id-type", "")
         id_value = get_safe(_id, "external-id-value", "")
 
-        # create source
-        source = {"id": f"{id_type}:{id_value}"}
+        # is id of particular "relationship" type
+        relationships = ["self", "version-of", "part-of"]
+        if not get_safe(_id, "external-id-relationship", "") in relationships:
+            return ""
 
-        # if not an id type that Manubot can cite, keep citation details
-        if id_type not in manubot_prefixes:
+        # is id that manubot is capable of citing
+        if id_type not in manubot_citable:
+            return ""
+
+        return f"{id_type}:{id_value}"
+
+    # go through each source
+    for work in response:
+        # list of ids in work
+        ids = []
+
+        # use "work-summary" field instead of top-level "external-ids" to reflect preferred sources
+        for summary in get_safe(work, "work-summary", []):
+            ids = ids + get_safe(summary, "external-ids.external-id", [])
+
+        # extract string ids from fields on work
+        ids = [get_id(_id) for _id in ids]
+
+        # pick first id that meets all criteria
+        _id = next((_id for _id in ids if _id), "")
+
+        # create source
+        source = {"id": _id}
+
+        # if no id that meets all criteria, keep citation details from orcid
+        if not _id:
             # get summaries
             summaries = get_safe(work, "work-summary", [])
 
